bc8a4790c947ddfee7007fee34cc4b3ed745e8d7
Andreas Lacza <andreas.lacza@me.com>
2022-04-24 19:46:28 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,14 +1,14 @@
 const copyright = document.getElementById("copyright");
 const year = new Date().getFullYear();
-copyright.innerText = `© ${year} Andreas Lacza`
+copyright.innerText = `© ${year} Andreas Lacza`;
 
 const dialog = document.getElementById("dialog");
 const open = document.getElementById("open");
 open.addEventListener("click", function () {
-    dialog.style.display = "block";
+  dialog.style.display = "block";
 });
 
 const close = document.getElementById("close");
 close.addEventListener("click", function () {
-    dialog.style.display = "none";
+  dialog.style.display = "none";
 });
